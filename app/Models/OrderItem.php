0ccaa304d793cd1f92568630c65313edc468f667
bikashpo <codewithamitgupta@gmail.com>
2024-06-09 08:29:57 +0545

    Creating model and model setup

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'order-id',
+        'product-id',
+        'quantity',
+        'unit_amount',
+        'total_amount',
+    ];
+
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+    public function Product(){
+        return $this->belongsTo(Product::class);
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'Category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_stock',
+    ];
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category(){
+        return $this->belongsTo(category::class);
+    }
+
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderItems(){
+        return $this->hasMany(OrderItem::class);
+    }
+}
